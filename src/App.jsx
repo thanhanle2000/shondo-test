f1a8f76f73ea4683d5d252b2987e4f0557e561ed
thanhanle2000 <55009973+thanhanle2000@users.noreply.github.com>
2024-10-19 16:10:42 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
-import { Router } from './core/component/router/Router'
+import './App.css';
+import Footer from './core/component/layout/Footer';
+import Header from './core/component/layout/Header';
+import { Router } from './core/component/router/Router';
+import './core/css/styles.less';
 
 function App() {
   return (
-    <>
-     <Router/>
-    </>
-  )
+    <div className="page">
+      <Header />
+      <div className="body">
+        <Router />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/core/component/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>My Website Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
